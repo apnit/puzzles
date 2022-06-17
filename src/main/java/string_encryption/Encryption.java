e0dac42cab6516b00039a8bc2a6c1b5b8350f8fc
ArmanGholizadehNIT <101034920+ArmanGholizadehNIT@users.noreply.github.com>
2022-06-17 22:32:34 +0430

    All Completed Except The Decryption

--- src/main/java/string_encryption/Encryption.java
+++ src/main/java/string_encryption/Encryption.java
@@ -2,10 +2,53 @@ package string_encryption;
 
 public class Encryption {
     public static String Encrypt(String str) {
-        return "";
+
+        String sum = "";
+        char[] holder = str.toCharArray();
+
+        if(str.length()%2 == 1) {
+            for(int i = 0 ; i< holder.length ; i++){
+
+                if( i != (holder.length-1) ){
+                    sum += (char)(holder[i] + holder[i + 1]);
+                }
+
+                else
+                {
+                    sum += (char)(holder[i] + holder[0]);
+                }
+            }
+
+        }
+        else {
+            for(int i = 0 ; i< holder.length ; i++){
+
+                if( i != (holder.length-1) ){
+                    sum += (char)(holder[i] + holder[i + 1]);
+                }
+
+                else
+                {
+                    sum += (holder[i]);
+                    sum += (holder[0]);
+                }
+            }
+        }
+
+
+
+        return sum;
     }
 
     public static String Decrypt(String str) {
+        char[] holder = str.toCharArray();
+        String sum = "";
+
+        for( int i = 0 ; i<str.length() ; i++){
+
+        }
+
+
         return "";
     }
 }
--- src/main/java/string_encryption/README.md
+++ src/main/java/string_encryption/README.md
@@ -12,7 +12,7 @@ The  algorithm works  on a string.  For each character in
  the string,  calculate  sum  of  its  ASCII (or unicode)
  value with ASCII value  of next  character. For the last
  character,  calculate   sum  of  its  value  with  first
- character.  Store  these  ASCCI  values in a new  string
+ character.  Store  these  ASCII  values in a new  string
  with the same size of the  initial string.
  
 To solve this puzzle complete these methods
