d9f2b0298abd22c1d151ac175c3bcc47928bc949
BuildTools <unconfigured@null.spigotmc.org>
2022-06-18 04:58:56 +0430

    just encryption yet

--- src/main/java/string_encryption/Encryption.java
+++ src/main/java/string_encryption/Encryption.java
@@ -2,10 +2,31 @@ package string_encryption;
 
 public class Encryption {
     public static String Encrypt(String str) {
-        return "";
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if (str.length() % 2 == 0) {
+            for (int i = 0; i < str.length(); i++) {
+                if (i == str.length() - 1) {
+                    stringBuilder.append(str.charAt(i)).append(str.charAt(0));
+                }else
+                    stringBuilder.append((char)((int) str.charAt(i) + (int) str.charAt(i+1)));
+            }
+        } else {
+            for (int i = 0; i < str.length(); i++) {
+                if (i == str.length() - 1) {
+                    stringBuilder.append((char)((int) str.charAt(i) + (int) str.charAt(0)));
+                } else
+                    stringBuilder.append((char)((int) str.charAt(i) + (int) str.charAt(i+1)));
+            }
+        }
+        return stringBuilder.toString();
     }
 
     public static String Decrypt(String str) {
-        return "";
+        StringBuilder stringBuilder = new StringBuilder();
+        /*
+        code
+         */
+        return stringBuilder.toString();
     }
 }
