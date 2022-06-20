11efb31b1fe8b85607f6473c2c481fb1ab1308c2
Mahdiyeh <101658092+mahdiyeh-mazani@users.noreply.github.com>
2022-06-20 13:12:28 +0430

    Update HatColor.java

--- src/main/java/prisoners_red_blue_hats/HatColor.java
+++ src/main/java/prisoners_red_blue_hats/HatColor.java
@@ -2,35 +2,35 @@ package prisoners_red_blue_hats;
 
 import java.util.Random;
 
-/**
- * Hat has a color
- * <p>
- * Red or Blue
- */
+///
+//        * Hat has a color
+//        * <p>
+// * Red or Blue
+//         */
 public enum HatColor {
     RED("RED"),
     BLUE("BLUE"),
     ;
 
-    /**
-     * String value of the enum values
-     */
+    //    /
+//            * String value of the enum values
+//     */
     private final String colorString;
 
-    /**
-     * Constructs an enum object
-     *
-     * @param colorString string value
-     */
+    //    /
+//            * Constructs an enum object
+//     *
+//             * @param colorString string value
+//     */
     HatColor(String colorString) {
         this.colorString = colorString;
     }
 
-    /**
-     * Makes a random color and returns it
-     *
-     * @return a random color
-     */
+    //    /
+//            * Makes a random color and returns it
+//     *
+//             * @return a random color
+//     */
     public static HatColor randomColor() {
         return (new Random().nextInt(2) == 0 ? RED : BLUE);
     }
