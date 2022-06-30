1f1d9445f236399fdbca5556af81653f27632fda
Alireza Jafartash <35873101+alirezaopmc@users.noreply.github.com>
2022-06-30 11:46:11 +0430

    Revert "just encryption yet"
    This reverts commit 566a886be0d08a84577c664a1684513106f37ac8.

--- src/main/java/string_encryption/Encryption.java
+++ src/main/java/string_encryption/Encryption.java
@@ -2,31 +2,10 @@ package string_encryption;
 
 public class Encryption {
     public static String Encrypt(String str) {
-        StringBuilder stringBuilder = new StringBuilder();
-
-        if (str.length() % 2 == 0) {
-            for (int i = 0; i < str.length(); i++) {
-                if (i == str.length() - 1) {
-                    stringBuilder.append(str.charAt(i)).append(str.charAt(0));
-                }else
-                    stringBuilder.append((char)((int) str.charAt(i) + (int) str.charAt(i+1)));
-            }
-        } else {
-            for (int i = 0; i < str.length(); i++) {
-                if (i == str.length() - 1) {
-                    stringBuilder.append((char)((int) str.charAt(i) + (int) str.charAt(0)));
-                } else
-                    stringBuilder.append((char)((int) str.charAt(i) + (int) str.charAt(i+1)));
-            }
-        }
-        return stringBuilder.toString();
+        return "";
     }
 
     public static String Decrypt(String str) {
-        StringBuilder stringBuilder = new StringBuilder();
-        /*
-        code
-         */
-        return stringBuilder.toString();
+        return "";
     }
 }
