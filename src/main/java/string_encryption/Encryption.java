ef0892217ebf7704f35a078e93d081764897294f
abolfazmz81 <101124457+abolfazmz81@users.noreply.github.com>
2022-07-13 18:35:10 +0430

    Update Encryption.java

--- src/main/java/string_encryption/Encryption.java
+++ src/main/java/string_encryption/Encryption.java
@@ -2,30 +2,53 @@ package string_encryption;
 
 public class Encryption {
     public static String Encrypt(String str) {
-        boolean even = str.length() % 2 == 0;
-        StringBuilder out = new StringBuilder();
-        for (int i=0; i < str.length(); i++) {
-            if (i == str.length()-1) {
-                if(even) out.append(str.charAt(i)).append(str.charAt(0));
-                else out.append((char)((int) str.charAt(i) + (int) str.charAt(0)));
+       String a = "";
+//        for (int i=0;i<str.length();i++){
+//            int s = (int)str.charAt(i) + (int)str.charAt(i) + 1 ;
+//            a += (char)s;
+//        }
+        for (int i=0;i<str.length();i++){
+
+            if (i== str.length()-1){
+                if (str.length()%2 == 0){
+                    a += (char)str.charAt(i) + "" + (char) str.charAt(0);
+                }
+                else {
+                    a += (char)( (int)str.charAt(i) + (int)str.charAt(0) );
+                }
             }
-            else out.append((char)((int) str.charAt(i) + (int) str.charAt(i+1)));
+            else {
+                a+= (char)( (int) str.charAt(i) + (int)str.charAt(i+1));
+            }
         }
-        return out.toString();
+        return a;
     }
 
     public static String Decrypt(String str) {
-        StringBuilder out = new StringBuilder();
+        String a = "";
         int sum = 0;
-        for (int i = 0; i < str.length(); i+=2) {
-            if (i == str.length()-1) sum += (int) str.charAt(i) - (int) str.charAt(0);
-            else sum += (int) str.charAt(i) - (int) str.charAt(i+1);
+        for (int i=0;i<str.length();i+=2){
+            if (i== str.length()- 1){
+                int b = str.charAt(i);
+                int c = str.charAt(0);
+                sum += b-c;
+            }
+            else {
+                int b= str.charAt(i);
+                int c = str.charAt(i+1);
+                sum+= b-c;
+            }
         }
-        char first = (char) ((sum + str.charAt(0))/2);
-        for (int i = 0; i < str.length(); i++) {
-            if (first != 0) out.append(first);
-            first = (char) (str.charAt(i) - first);
+
+        char cha =(char) ((sum + str.charAt(0)) /2);
+
+        for (int i=0;i<str.length();i++){
+            if (cha !=  0){
+                a+=cha;
+                cha =(char) (str.charAt(i) - cha);
+            }
         }
-        return out.toString();
+
+        return a;
     }
 }
